345b1a64d8bf7e27f1b2014baa516797463eafb1
Masaya Kinefuchi <kine0718@icloud.com>
2023-04-04 13:37:03 +0900

    サインアップ一次

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
-# from django.contrib.auth.models import AbstractUser
-# from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+    
 
+class User(AbstractUser):
+		email = models.EmailField() 
 
-# class User(AbstractUser):
 
 
 # class FriendShip(models.Model):
--- tweets/views.py
+++ tweets/views.py
@@ -1,1 +1,4 @@
-# from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = "tweets/home.html"
