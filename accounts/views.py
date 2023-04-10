0f66626fb5288946279b2ce0dd06cf0726bc8150
Masaya Kinefuchi <kine0718@icloud.com>
2023-04-10 09:57:34 +0900

    追加flak8修正

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
 
