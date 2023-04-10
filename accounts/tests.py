87b2d56346f8d32bf8c36d9e606c19b1dad8d7d1
Masaya Kinefuchi <kine0718@icloud.com>
2023-04-10 09:50:38 +0900

    flake8などの修正

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib.auth import SESSION_KEY, get_user_model
+from django.contrib.auth import SESSION_KEY
 from django.test import TestCase
 from django.urls import reverse
 
