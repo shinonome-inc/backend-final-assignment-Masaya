d7b7a1979caf7d6d8f33423e89f5817cf9e627f4
Masaya Kinefuchi <kine0718@icloud.com>
2023-04-11 18:05:00 +0900

    再修正

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.forms import UserCreationForm
-
 from accounts.models import User
 
 
--- tweets/views.py
+++ tweets/views.py
@@ -2,5 +2,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class HomeView(TemplateView, LoginRequiredMixin):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "tweets/home.html"
