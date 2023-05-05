0073725d65c89710c7b10ab9eab61b407514bb1e
Masaya Kinefuchi <kine0718@icloud.com>
2023-05-05 21:32:36 +0900

    CI

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class FollowUser(models.Model):
     follower = models.ForeignKey(User, related_name="follower", on_delete=models.CASCADE)
     following = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=["follower", "following"], name="unique_FollowUser"),
